40561781f33918f8efe93265f4c808f0555bbd59
ahnge <ahnge226@gmail.com>
2023-05-29 17:10:16 +0630

    Navbar that hids on scroll

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,47 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import logoImage from "../../public/images/logo.svg";
 import hamburgerImage from "../../public/images/icon-hamburger.svg";
 import MobileMenu from "./MobileMenu";
+import { debounce } from "../utilities/helpers";
 
 const Nav = () => {
+  // Mobile menu state
   const [open, setOpen] = useState(false);
+
+  // Sticky navbar that hides on scroll
+  // https://www.rolandwrites.com/blog/sticky-navbar-hides-scroll
+
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = debounce(() => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos > 70) ||
+        currentScrollPos < 10
+    );
+
+    setPrevScrollPos(currentScrollPos);
+  }, 100);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
     <nav>
-      <div className="w-full p-10 fixed top-0 mx-0">
+      <div
+        className={`w-full p-10 ${
+          visible ? "top-0" : "-top-32 shadow-sm"
+        } transition bg-neutral-white/5 backdrop-blur-md fixed mx-0`}
+      >
         <div className="flex justify-between items-center max-w-7xl mx-auto">
           <div>
             <Image src={logoImage} alt="Logo of the sunnyside agency" />
